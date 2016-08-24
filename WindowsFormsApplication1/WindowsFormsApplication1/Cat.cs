35e7c0d0d06c3fb7a4230357c8c9bcbc3f3fe70b
TeacherLv <168623972@qq.com>
2016-08-24 16:22:13 +0800

    增加相关内容

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Cat.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+
+    class Animal
+    {
+ 
+        //1、父类字段定义protected
+        protected  int  shoutNum = 0;
+        protected string name = "";
+
+        //2、显示的定义一个构造方法
+        public Animal()
+        {
+            this.name = "无名";
+
+        }
+        public Animal(string param1)
+        {
+            this.name = param1;
+        }
+        //3、定义shoutNum的属性
+        public int  ShoutNum
+        {
+            get
+            {
+                return shoutNum;
+            }
+            set
+            {
+                shoutNum = value;
+            }
+
+        }
+        //父类定义一个虚方法
+        public virtual string Shout()
+        {
+            return "";
+        }
+ 
+
+        
+        
+    }
+
+    
+    
+    
+    class Cat:Animal
+    {
+
+        //1、构造方法
+        public Cat():base()
+        {
+        }
+        public Cat(string param1):base(param1)
+        {
+        }
+
+        //2、子类个性化的方法
+        public override string Shout()
+        {
+            string result = "";
+            for (int i = 0; i < shoutNum; i++)
+            {
+                result = result  +"喵";
+                 
+            }
+            return "我的名字叫：" + name + result;
+        }
+    }
+
+    class Dog : Animal
+    {
+
+        //1、构造方法
+        public Dog()
+            : base()
+        {
+        }
+        public Dog(string param1)
+            : base(param1)
+        {
+        }
+
+        //2、子类个性化的方法
+        public override string Shout()
+        {
+            string result = "";
+            for (int i = 0; i < shoutNum; i++)
+            {
+                result = result + "汪";
+
+            }
+            return "我的名字叫：" + name + result;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//添加命名空间
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //批处理拷贝文件：拷贝文件夹下所有文件
+            string[] files = Directory.GetFiles("D:\\信息系统分析与设计\\2015-2016\\拷贝文件夹");
+            
+
+            //拷贝
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    //取文件名
+            //    string fileName = Path.GetFileName(files[i]);
+            //    //File.Copy("D:\\信息系统分析与设计\\2015-2016\\拷贝文件测试.txt", "D:\\拷贝文件测试.txt", true);
+            //    File.Copy(files[i], "D:\\拷贝目标文件夹\\" + fileName);
+        
+
+ 
+            //}
+
+            //删除
+            //for (int i = 0; i < files.Length; i++)
+            //foreach(string s in files)
+
+            //{
+            //    //取文件名
+            //    string fileName = Path.GetFileName(files[i]);
+            //    //File.Copy("D:\\信息系统分析与设计\\2015-2016\\拷贝文件测试.txt", "D:\\拷贝文件测试.txt", true);
+            //    //File.Copy(files[i], "D:\\拷贝目标文件夹\\" + fileName);
+            //    File.Delete("D:\\信息系统分析与设计\\2015-2016\\拷贝文件夹\\" + fileName);
+        
+
+ 
+            //}
+            //按行读取读取文件
+            StreamReader sr = new StreamReader("D:\\信息系统分析与设计\\2015-2016\\拷贝文件夹\\1.txt");
+            string sLine = "";
+
+            while ((sLine = sr.ReadLine()) != null) //关键代码：遍历
+            {
+                MessageBox.Show(sLine);
+            }
+
+ 
+
+
+            //File.Copy("D:\\信息系统分析与设计\\2015-2016\\拷贝文件测试.txt", "D:\\拷贝文件测试.txt",true);
+        }
+    }
+}
