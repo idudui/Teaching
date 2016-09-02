78fd296e159a3a472e150affc16ef9c12d262f7e
201415103 <2609342984@qq.com>
2016-09-02 20:46:39 +0800

    1

--- CSharp/201415103冯雨/ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ CSharp/201415103冯雨/ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("87b0d1ef-8fff-4b40-b775-e5464e54145f")]
+[assembly: Guid("da807200-6000-411a-86b9-65222fccce6b")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
