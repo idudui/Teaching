91b3a31c1c62889ec3c7d5cdd1d64f23127ea15b
z985 <741565218@qq.com>
2016-09-03 13:19:40 +0800

    实验一作业

---
+++ CSharp/201415225黑腾飞/实验一/作业4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 作业4//实验一：四.5 求55：88的累加和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum2 = 0;
+            for (int a2 = 55;
+                a2 > 54 && a2 <= 88;a2++)
+            {
+                sum2 += a2;
+            }
+        }
+        Console.WriteLine("55:88的累加和为" sum2 );
+    }
+}
---
+++ CSharp/201415225黑腾飞/实验一/实验一/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实验一//姓名：黑腾飞
+                //学号：201415225
+                //日期：2016.9.3
+                //实验一：一.5  输出“我会学好C#"
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我会学好C#");
+            Console.ReadKey();
+        }
+    }
+}
