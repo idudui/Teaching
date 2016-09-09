b3735a37b2e640fc05cb9326fd02e507b4e8685b
AloneAgain <348950572@qq.com>
2016-09-09 10:48:01 +0800

    实验作业

--- CSharp/201415111宋仕楠/实验作业/实验作业/Properties/AssemblyInfo.cs
+++ CSharp/201415111宋仕楠/实验作业/实验作业/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("实验一")]
+[assembly: AssemblyTitle("实验作业")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("实验一")]
+[assembly: AssemblyProduct("实验作业")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("0dfeb7e8-12a1-46ba-b88d-76d0849251c5")]
+[assembly: Guid("b27013bd-7682-495c-8d26-301a96a1f4c8")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
