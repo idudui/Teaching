130b3820b2f3600603697974ec74ff987b4449b4
ZeroZYB <1563080514@qq.com>
2016-09-09 10:53:27 +0800

    第二周作业及第一周作业修改

--- CSharp/201415113张亚彬/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ CSharp/201415113张亚彬/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -87,37 +87,36 @@ namespace ConsoleApplication1
 
 
 
-
             //第二周作业
             //1、反序打印九九乘法表
             int i, k;//定义两个变量
             //两个循环
-            for (i = 9; i >= 1; i -= 1)
+            for (i=9;i>=1;i--)
             {
-                for (k = 1; k <= i; k++)
+                for (k=1;k<=i;k++)
                 {
-                    Console.Write("{0}*{1}={2}\t", k, i, k * i);//占位符表示等式
+                    Console.Write("{0}*{1}={2}\t", k, i, k * i);//用占位符表示等式
                 }
                 Console.WriteLine();
             }
             Console.ReadKey();
 
 
-            //2、用循环画图
 
-            int x, y, j;//定义3个变量
-            for (x = 1; x <= 4; x++)//限定行数
+
+
+            //2、用循环画图
+            int x, y, j;//定义三个变量
+            for (x=1;x<=4;x++)//外循环定义行数
             {
-                for (y = 8 - 2 * x; y >= 0; y--)//打印空格
+                for (y=8-2*x;y>=0;y--)//打印空格
                 {
                     Console.Write(" ");
                 }
-                for (j = 1; j <= 2 * x - 1; j++)//打印星号
+                for (j=1;j<=2*x-1;j++)//打印星号
                 {
                     Console.Write("*");
                 }
-
-
                 Console.WriteLine();
             }
             Console.ReadKey();
@@ -127,24 +126,18 @@ namespace ConsoleApplication1
 
             //3、用循环画图
             int m, n;//定义两个变量
-
-            for (n = 1; n <= 4; n++)//限定行数
+            for (n=1;n<=4;n++)//限定行数
             {
-                for (m = 1; m <= 8; m++)//限定列数
+                for (m=1;m<=8;m++)//限定列数
                 {
-                    if (n % 2 != 0)//判断奇数行  偶数行
-
-                        Console.Write("*");
+                    if (n % 2 != 0)//判断是奇数行还是偶数行
+                        Console.Write("*");//奇数行打印*
                     else
-                        Console.Write("#");
+                        Console.Write("#");//偶数行打印#
                 }
-                   
-                Console.WriteLine();//每进行一次内循环换行
-
+                Console.WriteLine();
             }
             Console.ReadKey();
-
-
         }
     }
 }
